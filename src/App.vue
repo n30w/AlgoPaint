a1ff5f9fb3ea80530e399136caea9e8e3254c334
n30w <rocketneo@gmail.com>
2022-09-01 03:11:42 -1000

    Changed to Parcel and Vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div>AHAHSDIASDI WORK PLEASE GOD DAMN {{ name }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+let myName: string;
+myName = "brahhh"
+
+export default defineComponent({
+	data() {
+		return {
+			name: myName,
+		}
+	}
+})
+</script>

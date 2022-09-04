3d2ec22b60ef6f7239887d598162a973def0d6d1
n30w <rocketneo@gmail.com>
2022-09-04 01:52:58 -1000

    Quick cleanup, changed font

--- src/components/sketch/sketch.ts
+++ src/components/sketch/sketch.ts
@@ -6,13 +6,43 @@ export const sketch = (p5: P5) => {
 
     let cc: ComplexConstruct;
 
-    let s: Settings = {
-        sphereAmount: 20,
+    let s: Settings;
+    
+    // Cool Twisty Chain Thing
+    // s = {
+    //     sphereAmount: 20,
+    //     speedControl: 200,
+    //     constructMultiplier: 15,
+    //     detailX: 20,
+    //     detailY: 100,
+    //     startField: 22,
+    //     intensityMod: 2,
+    //     backgroundColor: 0,
+    //     renderColor: false,
+    //     renderSmear: false
+    // };
+    
+    // Curvy Staircases
+    // s = {
+    //     sphereAmount: 10,
+    //     speedControl: 200,
+    //     constructMultiplier: 2,
+    //     detailX: 10,
+    //     detailY: 2,
+    //     startField: 0,
+    //     intensityMod: 2,
+    //     backgroundColor: 0,
+    //     renderColor: false,
+    //     renderSmear: false
+    // };
+
+    s = {
+        sphereAmount: 40,
         speedControl: 200,
-        constructMultiplier: 15,
-        detailX: 20,
-        detailY: 100,
-        startField: 22,
+        constructMultiplier: 2,
+        detailX: 10,
+        detailY: 2,
+        startField: 0,
         intensityMod: 2,
         backgroundColor: 0,
         renderColor: false,
@@ -28,6 +58,7 @@ export const sketch = (p5: P5) => {
     };
 
     p5.draw = () => {
+        // Perhaps I can add some kind of light somewhere?
         cc.createConstruct();        
     };
 };

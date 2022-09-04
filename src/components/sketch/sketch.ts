80d4740f3e8a700d7346191c8063671018ba2973
n30w <rocketneo@gmail.com>
2022-09-04 02:38:32 -1000

    Added preset map

--- src/components/sketch/sketch.ts
+++ src/components/sketch/sketch.ts
@@ -6,59 +6,84 @@ export const sketch = (p5: P5) => {
 
     let cc: ComplexConstruct;
 
-    let s: Settings;
-    
-    // Cool Twisty Chain Thing
-    // s = {
-    //     sphereAmount: 20,
-    //     speedControl: 200,
-    //     constructMultiplier: 15,
-    //     detailX: 20,
-    //     detailY: 100,
-    //     startField: 22,
-    //     intensityMod: 2,
-    //     backgroundColor: 0,
-    //     renderColor: false,
-    //     renderSmear: false
-    // };
-    
-    // Curvy Staircases
-    // s = {
-    //     sphereAmount: 10,
-    //     speedControl: 200,
-    //     constructMultiplier: 2,
-    //     detailX: 10,
-    //     detailY: 2,
-    //     startField: 0,
-    //     intensityMod: 2,
-    //     backgroundColor: 0,
-    //     renderColor: false,
-    //     renderSmear: false
-    // };
+    // Built-in presets! Map makes it easier to select :)
+    let presets = new Map<string, Settings>([
+        [
+            "twist_chain", 
+            {   // Cool Twisty Chain Thing
+                sphereAmount: 20,
+                speedControl: 200,
+                constructMultiplier: 15,
+                detailX: 20,
+                detailY: 100,
+                startField: 22,
+                intensityMod: 2,
+                backgroundColor: 0,
+                renderColor: false,
+                renderSmear: false
+            }
+        ],
 
-    s = {
-        sphereAmount: 40,
-        speedControl: 200,
-        constructMultiplier: 2,
-        detailX: 10,
-        detailY: 2,
-        startField: 0,
-        intensityMod: 2,
-        backgroundColor: 0,
-        renderColor: false,
-        renderSmear: false
-    };
+        [
+            "twist_chain_2", 
+            {
+                sphereAmount: 20,
+                speedControl: 200,
+                constructMultiplier: 15,
+                detailX: 20,
+                detailY: 100,
+                startField: 22,
+                intensityMod: 2,
+                backgroundColor: 0,
+                renderColor: false,
+                renderSmear: false
+            }
+        ],
+
+        [
+            "curvy_stairs", 
+            {
+                sphereAmount: 10,
+                speedControl: 200,
+                constructMultiplier: 2,
+                detailX: 10,
+                detailY: 2,
+                startField: 0,
+                intensityMod: 2,
+                backgroundColor: 0,
+                renderColor: false,
+                renderSmear: false
+            }
+        ], 
+
+        [
+            "wonderful_rect", 
+            {
+                sphereAmount: 90,
+                speedControl: 200,
+                constructMultiplier: 23,
+                detailX: 1,
+                detailY: 2,
+                startField: 0,
+                intensityMod: 2,
+                backgroundColor: 0,
+                renderColor: false,
+                renderSmear: false
+            }
+        ], 
+    ]);
 
     p5.setup = () => {
         const canvas = p5.createCanvas(1000, 1000, "webgl");
         canvas.parent("p5Canvas");
-        p5.background(s.backgroundColor);
+        p5.background(0);
         p5.colorMode(p5.HSL, 255);
-        cc = new ComplexConstruct(p5, s);
+        cc = new ComplexConstruct(p5, presets.get("wonderful_rect"));
     };
 
     p5.draw = () => {
         // Perhaps I can add some kind of light somewhere?
+        // Create a save frame function, viewer can take a picture of their painting
         cc.createConstruct();        
     };
 };
